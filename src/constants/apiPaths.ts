f265fca00f31d35e238e7c9d43252694432001d8
Vitaly <kaseopea@gmail.com>
2020-11-14 15:52:30 +0300

    Task 5.1

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://dna4dmh8vb.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://dna4dmh8vb.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://dna4dmh8vb.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://0ywl81yhwd.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://dna4dmh8vb.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
