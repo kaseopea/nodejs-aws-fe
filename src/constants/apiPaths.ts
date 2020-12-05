2a6074f9a5c9d538ab35a76a2ad9e603a07aac84
Vitali_Rusau <Vitali_Rusau@epam.com>
2020-12-05 23:21:41 +0300

    added cart path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,8 @@ const API_PATHS = {
   product: 'https://dna4dmh8vb.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://dna4dmh8vb.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://y52kp67m6a.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://dna4dmh8vb.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://dna4dmh8vb.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://kaseopea-cart-api-dev.eu-aest-1.elasticbeanstalk.com/api',
 };
 
 export default API_PATHS;
